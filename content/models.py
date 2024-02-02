50ffaf1d4c8583e9f5a1ade314f2424111a9533d
ColinAdo <colinadodev30@gmail.com>
2024-02-02 21:07:11 +0300

    Add Post model

--- content/models.py
+++ content/models.py
@@ -10,3 +10,9 @@ class CustomUser(AbstractUser):
 # Modify the ManyToManyField related names to avoid clashes
 CustomUser.groups.field.remote_field.related_name = 'customuser_groups'
 CustomUser.user_permissions.field.remote_field.related_name = 'customuser_user_permissions'
+
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField(blank=True, null=True)
