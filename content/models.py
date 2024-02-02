de78154548e479b223c4449146774600a4e03f3f
ColinAdo <colinadodev30@gmail.com>
2024-02-02 21:04:23 +0300

    Update  custom user model proper

--- content/models.py
+++ content/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    follower = models.ManyToManyField('self', related_name='followees', symmetrical=False)
+    followers = models.ManyToManyField(
+        'self', related_name='followees', symmetrical=False)
+
+
+# Modify the ManyToManyField related names to avoid clashes
+CustomUser.groups.field.remote_field.related_name = 'customuser_groups'
+CustomUser.user_permissions.field.remote_field.related_name = 'customuser_user_permissions'
