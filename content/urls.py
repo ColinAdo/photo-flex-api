2d85e7d8077646d82573f13d82680fafcc3249d1
ColinAdo <colinadodev30@gmail.com>
2024-02-03 09:11:15 +0300

    Update URL endpoint

--- content/urls.py
+++ content/urls.py
@@ -1,29 +1,19 @@
-from django.utils import path
+from django.urls import path
 
 from .views import (
     UserListView,
     UserDetailView,
     UserPostListView,
-
-    PostListView,
-    PostDetailView,
-    PostPhotoListView,
-
     PhotoListView,
-    PhotoDetailView,
+
 )
 
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('users/<int:pk>/posts/', UserPostListView.as_view(), name='userpost-list'),
+    path('users/<int:pk>/posts/<int:post_pk>/photos/', PhotoListView.as_view(), name='photos-list'),
 
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/photos/', PostPhotoListView.as_view(), name='postphoto-list'),
-
-    path('photos/', PhotoListView.as_view(), name='photo-list'),
-    path('photos/<int:pk>/', PhotoDetailView.as_view(), name='photo-detail'),
 
 
 ]
--- photoflexapi/urls.py
+++ photoflexapi/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('photoflexapi.urls')),
+    path('', include('content.urls')),
 ]
